ec3ada2cdd55b41f7737a29d8f5e1202b445c1b5
Anirudh <anirudhvsp@gmail.com>
2026-08-13 19:59:08 +0530

    fixed error

--- services/jobs.service.js
+++ services/jobs.service.js
@@ -50,13 +50,13 @@ async function addHtmlIncrementalSkillJob({
   });
 }
 
-async function addHtmlSkillPreviewJob({ ticker, data_extraction_prompt, html_template_prompt, enable_data_validation, data_validation_loops, enable_html_validation, transcript_signal_types, ppt_signal_types, annual_report_signal_types, extraction_model, fact_validation_model, html_template_model, visual_qa_extraction_model, fact_validation_model, html_template_model, visual_qa_model, max_tokens, max_transcript_qtrs, max_ppt_qtrs, max_annual_report_years, market_data_signal_types = [], max_market_data_months = null, force = false }) {
+async function addHtmlSkillPreviewJob({ ticker, data_extraction_prompt, html_template_prompt, enable_data_validation, data_validation_loops, enable_html_validation, transcript_signal_types, ppt_signal_types, annual_report_signal_types, extraction_model, fact_validation_model, html_template_model, visual_qa_model, max_tokens, max_transcript_qtrs, max_ppt_qtrs, max_annual_report_years, market_data_signal_types = [], max_market_data_months = null, force = false }) {
   return jobQueue.addJob('html_skill_preview', {
     ticker, data_extraction_prompt, html_template_prompt, enable_data_validation, data_validation_loops, enable_html_validation,
     transcript_signal_types:    transcript_signal_types    ?? [],
     ppt_signal_types:           ppt_signal_types           ?? [],
     annual_report_signal_types: annual_report_signal_types ?? [],
-    model, max_tokens,
+    extraction_model, fact_validation_model, html_template_model, visual_qa_model, max_tokens,
     max_transcript_qtrs, max_ppt_qtrs, max_annual_report_years,
     market_data_signal_types: market_data_signal_types ?? [],
     max_market_data_months,
