534459ccff751852fb6f5826f13aeef46fbb1025
Anirudh <anirudhvsp@gmail.com>
2026-08-20 17:42:24 +0530

    backend fix

--- services/jobs.service.js
+++ services/jobs.service.js
@@ -273,7 +273,6 @@ module.exports = {
   addHtmlIncrementalSkillJob,
   addHtmlCompressedSkillJob,
   findJob,
-  cleanupQueues,
   addHtmlIncrementalRegenerateJob,
   addHtmlCompressedRegenerateJob,
 };
