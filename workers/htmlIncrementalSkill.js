2da11ec7cb9d77c78885dfe0a5a4eadaa576b0c8
Anirudh <anirudhvsp@gmail.com>
2026-08-25 16:00:14 +0530

    update worker count

--- workers/htmlIncrementalSkill.js
+++ workers/htmlIncrementalSkill.js
@@ -47,8 +47,8 @@ async function processHtmlIncrementalSkillJob(job) {
 
 const worker = new Worker('html_skill_incremental', processHtmlIncrementalSkillJob, {
   connection,
-  concurrency: 10,
-  limiter: { max: 10, duration: 1000 },
+  concurrency: 20,
+  limiter: { max: 20, duration: 1000 },
 });
 
 worker.on('completed', (job)      => console.log(`[htmlIncrementalSkill] Job ${job.id} completed`));
